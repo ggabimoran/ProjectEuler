ca6f7192909e8e149d8d098991cd8dbe8b7dec55
ggabimoran <gabrielmoran7@live.fr>
2020-01-22 20:03:15 +0100

    improved euler1 solution

--- euler_001/euler1.cpp
+++ euler_001/euler1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cassert>
 
 using namespace std;
 
@@ -9,12 +10,24 @@ La somme en question est égal à :
 Puis on applique la formule \sum_{k=1}^N = N(N+1)/2
  */
 
+int sum_mult_under(int multiple,int bound){
+  assert(multiple!=0);
+  int quotient=bound/multiple;
+  int n_terms=(bound % multiple == 0) ? (quotient - 1) : quotient;
+  return (multiple * n_terms * (n_terms + 1) / 2);
+}
+
 int main(){
   clock_t begin = clock();
-  int s = 3*333*334/2 + 5*199*200/2 - 15*66*67/2;
+
+  
+  int bound=1000;
+  int res = sum_mult_under(3,bound) + sum_mult_under(5,bound) - sum_mult_under(15,bound);
+  cout<< res <<endl;
+
+  
   clock_t end = clock();
   double time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
-  cout<<s<<endl;
   cout<<"Execution time :"<<time_spent<<endl;
   return 0;
 }
