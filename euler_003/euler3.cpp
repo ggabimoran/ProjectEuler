db739cdcbba9797f7c6c9b1fba4caa2180db645f
ggabimoran <gabrielmoran7@live.fr>
2020-01-23 12:38:35 +0100

    improved euler003

--- euler_003/euler3.cpp
+++ euler_003/euler3.cpp
@@ -1,33 +1,35 @@
 #include<iostream>
 #include<cmath>
+#include<cassert>
 
-using namespace std;
-
-int is_prime(int n){
-  int i;
-  if (n <= 1){
-    return 0;
-  }
-  for (i=2;i<(int)sqrt(n)+1;i++){
-    if (n % i == 0){
-      return 0;
-    }
-  }
-  return 1;
+bool is_prime(std::int_fast64_t n){
+  if (n <= 1) return false;
+  for (int i=2;i<(std::floor(std::sqrt(n))+1);++i)
+    if (n % i == 0) return false;
+  return true;
 }
 
-int largest_prime_factor(unsigned long long int n){
+std::int_fast64_t largest_prime_factor(std::int_fast64_t n){
+  assert(n>=2);
   int i=2;
   while (!is_prime(n)){
-    while(n%i != 0 || !is_prime(i)){
-      i++;
-    }
-    n = n/i;
+    //prime factor decomposition ensures i will be prime whenever
+    //following statement evaluates to true.
+    //second condition ensures while loop termination
+    while(n%i == 0 && n!=i) n/=i;
+    ++i;
   }
   return n;
 }
 
 int main(){
-  cout<<largest_prime_factor(600851475143)<<endl;
+  clock_t begin = clock();
+
+  
+  std::cout << largest_prime_factor(600851475143) << '\n';
+
+  clock_t end = clock();
+  double time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
+  std::cout<<"Execution time :"<<time_spent<<'\n';
   return 0;
 }
